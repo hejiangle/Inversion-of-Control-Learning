cbbbd3ae78e90dcdcc437e527e094e318abbdfe5
hejiangle <jlhe@thoughtworks.com>
2019-07-09 19:17:28 +0800

    Add some comment to explain codes.

--- SimpleIoCContainer/Program.cs
+++ SimpleIoCContainer/Program.cs
@@ -10,9 +10,11 @@ namespace SimpleIoCContainer
             Utilities.SimpleIoCContainer.Register();
             Utilities.SimpleIoCContainer.Register<StudentController>();
 
+//          We can consider the following two lines are framework level code in a real Project.
 //            var studentController = (StudentController)Utilities.SimpleIoCContainer.Resolve<StudentController>();
             var studentController = (StudentController)Utilities.SimpleIoCContainer.ResolveDependant<StudentController>();
 
+//          This line is mocking a request to access the specific controller method.
             var allAbove90ScoresFemaleStudents = studentController.GetAllAbove90ScoresFemaleStudent();
 
             foreach (var student in allAbove90ScoresFemaleStudents)
